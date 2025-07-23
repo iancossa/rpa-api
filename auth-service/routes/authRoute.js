57eeecc857de85d28506eda58b915c2cf31e078d
iancossa <ianrafaelelias@gmail.com>
2025-07-23 20:01:23 +0530

    protecting routes

---
+++ auth-service/routes/authRoute.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const {signup, login} = require('../controllers/authController');
+
+const router = express.Router();
+// POST /signup
+router.post('/signup', signup);
+// POST /login  
+router.post('/login', login);
+
+//protect routes with auth middleware
+const authMiddleware = require('../middleware/authMiddleware');
+router.use(authMiddleware);
+
+router.get('/profile',authMiddleware,(req,res)=>{
+    res.json({user: req.user})
+})
+
+module.exports = router;
