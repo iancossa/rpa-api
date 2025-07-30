abc2d005a82f6c4fb45a70290992acc3f92cc086
iancossa <ianrafaelelias@gmail.com>
2025-07-30 23:59:08 +0530

    errorHandlinng

---
+++ user-service/utils/customError.js
@@ -0,0 +1,12 @@
+//it gives me a good control over error handling in my application
+
+class CustomError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = CustomError;
